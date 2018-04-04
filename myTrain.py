fdce7e188768d664ce32dd291b2c395247e7b36c
jajajang <ksajks@naver.com>
2018-04-05 03:50:31 +0900

    cuda thingy

--- myTrain.py
+++ myTrain.py
@@ -223,7 +223,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
-        loss.backward()
+        loss.backward(retain_graph=True)
         optimizer.step()
 
         # measure elapsed time
