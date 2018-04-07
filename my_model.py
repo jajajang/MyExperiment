0176c64b96238e201a46eb280d5469ef54473f0d
jajajang <ksajks@naver.com>
2018-04-07 17:59:36 +0900

    Fix loss

--- my_model.py
+++ my_model.py
@@ -185,8 +185,7 @@ class whereru(Function):
         input=Variable(inputt, requires_grad=False)
         for j in range(0,position.size()[0]):
             zet=(th.sqrt(th.sum(input*input, dim=-1)+1))
-            print th.sum(input*position[j], dim=-1)
-            print zet*position_zet[j]
+            print zet*position_zet[j]-th.sum(input*position[j], dim=-1)
             resulty[j]=-self.ruler(zet*position_zet[j]-th.sum(input*position[j], dim=-1)).cuda()
         return resulty
 
