d5f000088d55c7b923c50b0cb4371efd9d4b20a5
jajajang <ksajks@naver.com>
2018-05-05 03:04:55 +0900

    152 works?

--- my_model.py
+++ my_model.py
@@ -44,6 +44,43 @@ class BasicBlock(nn.Module):
         out = self.relu(out)
         return out
 
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * 4)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
 
 class myResNet(nn.Module):
 
@@ -113,6 +150,9 @@ def myResnet18(pretrained=False, **kwargs):
     model = myResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
     return model
 
+def myResnet152(pretrained=False, **kwargs):
+    model = myResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
+    return model
 
 eps=1e-5
 class PoincareDistance(Function):
