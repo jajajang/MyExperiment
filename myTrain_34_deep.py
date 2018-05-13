87591da12b9fbd76066e064d744b272ed29fb896
jajajang <ksajks@naver.com>
2018-05-14 01:25:30 +0900

    distance measure

--- myTrain_34_deep.py
+++ myTrain_34_deep.py
@@ -39,7 +39,7 @@ model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
 
-class MyImageFolder(datasetsImageFolder):
+class MyImageFolder(datasets.ImageFolder):
     def __getitem__(self, index):
         return super(MyImageFolder, self).__getitem__(index), self.imgs[index]#return image path
 
