4767b8289fd547a7d767a346a94458f4e2ea2de5
jajajang <ksajks@naver.com>
2018-05-15 04:41:40 +0900

    massive change for higher state

--- my_model.py
+++ my_model.py
@@ -303,6 +303,7 @@ class myLossV(Moddy._WeightedLoss):
         self.ignore_index = ignore_index
         self.reduce = reduce
     def forward(self, input, target):
+        Moddy._assert_no_grad(target)
         all=(target.data).expand(len(input))
         zet=((th.sqrt(th.sum(input*input, dim=-1)+1)))
         inner=(th.sum(input*position_all[all], dim=-1))
