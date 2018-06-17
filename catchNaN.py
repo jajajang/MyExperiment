8e2a8c70ab68e75a1035e19b7f8b2f33dffb8fb7
jajajang <ksajks@naver.com>
2018-06-18 06:58:00 +0900

    catchNaN

--- catchNaN.py
+++ catchNaN.py
@@ -227,7 +227,7 @@ def train(train_loader, model, criterion, criterion2, criterion3, optimizer, epo
 
         loss = criterion(output, target_var, level)
         output2 = criterion2(output)
-        if torch.isnan(loss.cpu()):
+        if loss.cpu()!=loss.cpu():
             outf_nan.write('\n')
             outf_nan.write('input : \n'+str(target))
             outf_nan.write('output : \n'+str(output))
