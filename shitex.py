8aa5e4943f10e2c0d2721a087a197db85402714c
jajajang <ksajks@naver.com>
2018-07-08 21:57:53 +0900

    time and nan

--- shitex.py
+++ shitex.py
@@ -1,5 +1,6 @@
 import torch as th
 from nltk.corpus import wordnet as wn
+import numpy as np
 z_dim=5
 indy_mix=[]
 ordered_word=[]
@@ -26,6 +27,12 @@ zetty=-th.sum(true_pos*true_pos, dim=-1).expand(z_dim,-1)+1
 position=2*true_pos/zetty.t()
 position_zet= (th.sqrt(th.sum(position*position, dim=-1)+1))
 
+
+zetty_all=(-th.sum(position_*position_, dim=-1).expand(z_dim,-1)+1)
+position_all=2*position_/zetty_all.t()
+position_all_zet= (th.sqrt(th.sum(position_all*position_all, dim=-1)+1))
+
+
 synsetss=[0]*len(obj)
 for i in range(0,len(obj)):
     synsetss[i]=wn.synset(obj[i])
@@ -38,24 +45,32 @@ for i in range(0,len(obj)):
         if synsetss[j] in pathy:
             level[i]+=1
 
-bitbit=[0]*7
-for i in range(2,7):
+bitbit=[0]*12
+for i in range(2,12):
     bitbit[i]=[j for j,x in enumerate(level) if x==i]
 
 ordy=[0]*200
 for i in range(0,200):
     ordy[i]=wn.synset(ordered_word[i])
 
-categorize=[0]*7
-for i in range(0,7):
+categorize=[0]*12
+for i in range(0,12):
     categorize[i]=[0]*200
 
-for s in range(2,6):
+for s in range(2,12):
     for i in range(0,200):
         pathy=ordy[i].hypernym_paths()[0]
+        tik=True
         for j in bitbit[s]:
             if synsetss[j] in pathy:
                 categorize[s][i]=j
+                tik=False
+            if tik:
+                categorize[s][i]=categorize[s-1][i]    
 
-for i in range(0,200):
-    categorize[6][i]=obj.index(ordered_word[i])
+for s in range(2,12):
+    level_sum=0
+    for i in range(0,200):
+        x=position_all_zet[categorize[s][i]]
+        level_sum+=np.log(x+np.sqrt(x*x+1))
+    print level_sum/200
