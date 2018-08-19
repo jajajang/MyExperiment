fc0c9ca640452c6bc88868aed0838a1985fc0cc2
jajajang <ksajks@naver.com>
2018-08-20 07:04:30 +0900

    shit

---
+++ 180820_top1_level10.py
@@ -0,0 +1,335 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+import argparse
+import gc
+import logging
+from collections import defaultdict as ddict
+
+import numpy as np
+import torch as th
+import torch.multiprocessing as mp
+from sklearn.metrics import average_precision_score
+from torch.autograd import Variable
+
+import my_model as my_modell
+
+import argparse
+import os
+import shutil
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+class MyImageFolder(datasets.ImageFolder):
+    def __getitem__(self, index):
+        return super(MyImageFolder, self).__getitem__(index), self.imgs[index]#return image path
+
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
+                    choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet18)')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--print-freq', '-p', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--world-size', default=1, type=int,
+                    help='number of distributed processes')
+parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
+                    help='url used to set up distributed training')
+parser.add_argument('--dist-backend', default='gloo', type=str,
+                    help='distributed backend')
+
+best_prec1 = 0
+outf_mean=open('mean_error.txt','w')
+outf_center=open('centy.txt','w')
+
+
+def main():
+    global args, best_prec1
+    args = parser.parse_args()
+
+    args.distributed = args.world_size > 1
+
+    if args.distributed:
+        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                world_size=args.world_size)
+
+    # create model
+    if args.pretrained:
+        print("=> using pre-trained model '{}'".format(args.arch))
+        model = my_modell.myResnet18(pretrained=True)
+
+    else:
+        print("=> creating model '{}'".format(args.arch))
+        #model = my_modell.myResnet34deep()
+        model = my_modell.myResnet18(pretrained=True)
+
+
+    if not args.distributed:
+        if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
+            model.features = torch.nn.DataParallel(model.features)
+            model.cuda()
+        else:
+            model = torch.nn.DataParallel(model).cuda()
+    else:
+        model.cuda()
+        model = torch.nn.parallel.DistributedDataParallel(model)
+
+    # define loss function (criterion) and optimizer
+    criterion_ = my_modell.myLossL().cuda()
+    criterion2_ = my_modell.myLossAL().cuda()
+    criterion3_ = my_modell.myLoss().cuda()
+    criterionC_ = my_modell.myLossC().cuda()
+
+    if args.pretrained:
+        ignored_params = list(map(id, model.module.fc_mine.parameters()))
+        base_params = filter(lambda p: id(p) not in ignored_params,
+                            model.module.parameters())
+        for paramy in base_params:
+            paramy.requires_grad=False
+        optimizer = torch.optim.Adam(model.module.fc_mine.parameters(), args.lr,
+                                weight_decay=args.weight_decay)
+
+    else: optimizer = torch.optim.Adam(model.parameters(), args.lr,
+                                weight_decay=args.weight_decay)
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    cudnn.benchmark = True
+
+    # Data loading code
+    traindir = os.path.join(args.data, 'train')
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    train_dataset = MyImageFolder(
+        traindir,
+        transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+
+    val_dataset = MyImageFolder(
+        valdir,
+        transforms.Compose([
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+    if args.distributed:
+        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    else:
+        train_sampler = None
+
+    val_sampler = None
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+        num_workers=args.workers, pin_memory=True, sampler=train_sampler)
+
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
+        num_workers=args.workers, pin_memory=True, sampler=val_sampler)
+
+    if args.evaluate:
+        #validate(val_loader, model, criterion2)
+        return
+    level=9
+    for epoch in range(args.start_epoch, args.epochs):
+        if args.distributed:
+            train_sampler.set_epoch(epoch)
+        if epoch%200==0:
+            level=level+1
+            print 'Hello world!'+str(level)
+        adjust_learning_rate(optimizer, epoch%200*(0.5**(level-5)))
+        # train for one epoch
+        train(train_loader, model, criterion_, criterion2_, optimizer, epoch, level)
+    torch.save(model.module.state_dict(),'mytraining.pt')
+
+def train(train_loader, model, criterion, criterion2, optimizer, epoch, level):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = [0]*13
+    top5 = [0]*13
+    for i in range(0,13):
+        top1[i]=AverageMeter()
+        top5[i]=AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, datata in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        (input, target), (filenam,_)=datata
+
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input)
+        target_var = torch.autograd.Variable(target)
+
+        # compute output
+        output = model(input_var)
+
+        loss = criterion(output, target_var, level)
+
+        # measure accuracy and record loss
+        losses.update(loss.data[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (epoch%200>180) or (epoch%200<20):
+            for levy in range(6,13):
+                output2 = criterion2(output, levy)
+                prec1,= accuracy_level(output2.data, target, levy)
+                top1[levy].update(prec1)
+                if i % args.print_freq ==0:
+                    print('Level {levy} - Top1 {top1.val:.3f}({top1.avg:.3f})\t'.format(levy=levy, top1=top1[levy]))
+
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses))
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+def accuracy_level(output, target, level, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    """This function cares for same elements"""
+    round_dist=th.FloatTensor(np.round(output.cpu().numpy(),decimals=4))
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    perf_targ=my_modell.categorize[level][target]
+    correct_slot=[0]*maxk
+
+    for i in range(0, batch_size):
+        _, treat_index=round_dist[i].unique(sorted=True, return_inverse=True)
+        for topi in range(0,maxk):
+            first_guy=(treat_index==topi).nonzero()[0][0]
+            if my_modell.categorize[level][first_guy]==perf_targ[i]:
+                correct_slot[topi]+=1
+    res = []
+    for k in topk:
+        correct_k = sum(correct_slot[:k])
+        res.append(correct_k*(100.0 / batch_size))
+    return res
+
+
+if __name__ == '__main__':
+    main()
