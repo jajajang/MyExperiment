520684f5145ad1790f0f204ee1be52c0491c4966
jajajang <ksajks@naver.com>
2018-08-25 06:36:20 +0900

    softmax

--- 180825_softmax_dim2.py
+++ 180825_softmax_dim2.py
@@ -226,7 +226,7 @@ def train(train_loader, model, criterion, optimizer, epoch, level):
         disty = criterion(output)
         loss=nn.CrossEntropyLoss(disty,target)
         # measure accuracy and record loss
-        losses.update(loss, input.size(0))
+        losses.update(loss.item(), input.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
