d8e8ea5101d344f8896730d0a5b648a9f4132e32
jajajang <ksajks@naver.com>
2018-08-25 07:01:57 +0900

    softmax

--- 180825_softmax_dim2.py
+++ 180825_softmax_dim2.py
@@ -237,13 +237,8 @@ def train(train_loader, model, criterion, crossme, optimizer, epoch, level):
         if (epoch%100>90) or (epoch%100<10):
             output2 = criterion(output)
             prec1, prec5 = accuracy(output2.data, target, topk=(1, 5))
-            
-            top1.update(prec1)
-            top5.update(prec5)
-            print top5.avg
-            print top5.val
-            print top1.avg
-            print top1.val
+            top1.update(prec1[0])
+            top5.update(prec5[0])
             if i % args.print_freq ==0:
                 print('Accu - Top1 {top1.val:.3f}({top1.avg:.3f})\t'
                 'Top5 {top5.val:.3f}({top5.avg:.3f})\t'.format(top1=top1, top5=top5))
