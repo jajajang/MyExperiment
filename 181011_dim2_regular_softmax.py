d6f2918e3fa999daa38f9e8eec82a615af3f3665
jajajang <ksajks@naver.com>
2018-10-11 05:14:25 +0900

    shit

--- 181011_dim2_regular_softmax.py
+++ 181011_dim2_regular_softmax.py
@@ -224,7 +224,7 @@ def train(train_loader, model, crit, criterion, criterion2, criterionC, optimize
         forprint = criterionC(output)
 
 
-        loss = crit(F.log_softmax(criterion(output)), target_var)-forprint
+        loss = crit(F.softmax(criterion(output)), target_var, log_input=False)-forprint
         # measure accuracy and record loss
         losses.update(loss.item(), input.size(0))
 
