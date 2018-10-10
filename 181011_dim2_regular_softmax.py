b3353a97263b252e93e6d33fc034e19626ed9820
jajajang <ksajks@naver.com>
2018-10-11 04:44:02 +0900

    shit

--- 181011_dim2_regular_softmax.py
+++ 181011_dim2_regular_softmax.py
@@ -254,7 +254,7 @@ def train(train_loader, model, crit, criterion, criterion2, criterionC, optimize
                   'forprint {forprint:.3f}\t'.format(
                    epoch, i, len(train_loader), batch_time=batch_time,
                    data_time=data_time, loss=losses, forprint = forprint))
-            outf_center.write(str(forprint.data[0]+'\n'))       
+            outf_center.write(str(forprint.data[0])+'\n')       
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
